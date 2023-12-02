313309f43faa17c21dbbd44d609a858c75d96076
sukrit101 <sukritsaha27@gmail.com>
2023-12-02 15:08:21 +0530

    improving typournament rules

--- src/config/index.ts
+++ src/config/index.ts
@@ -18,26 +18,29 @@ export const rules = [
   },
   {
     title: "Step 2: Tournament Structure",
-    steps: ["Each tournament consists of 3 rounds between 2 participants."],
+    steps: [
+      "Tournaments are organized in a league format.",
+      "Each match within the tournament consists of 3 rounds between 2 participants.",
+    ],
   },
   {
     title: "Step 3: Round 1 - Shared Typing Test",
     steps: [
       "Both participants gather.",
-      "Perform a coin toss to decide whose laptop/pc will be used.",
-      "Another coin toss determines who goes first.",
-      "Take a typing test on the chosen participant's device.",
+      "Perform a coin toss.",
+      "The winner of the toss gets to decide who types first or whose device is used first.",
+      "The loser gets to decide on the remaining option.",
     ],
   },
   {
     title: "Step 4: Round 2 - Switching Sides",
     steps: [
-      "Repeat the process with the roles reversed.",
-      "Participants type on the other participant's laptop/pc.",
+      "The winner of Round 1 automatically wins the toss for Round 2.",
+      "The rules for deciding the rest remains the same.",
     ],
   },
   {
-    title: "Step 5: Round 3 - Solo Typing Test",
+    title: "Step 5: Round 3 - Comfort Zone Typing Test",
     steps: [
       "Both participants return to their own devices.",
       "Take an individual typing test.",
@@ -47,13 +50,13 @@ export const rules = [
     title: "Step 6: Score Entry",
     steps: [
       "After each round, enter the scores for both participants.",
-      "The application will handle the rest.",
+      "Leave the rest to us.",
     ],
   },
   {
     title: "Step 7: Enjoy the Excitement!",
     steps: [
-      "Sit back and let the application process the results.",
+      "Sit back and let us process the results.",
       "Witness the thrill of the Typing Tournament unfold.",
     ],
   },
