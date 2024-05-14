387cd248396ce90220a40a04631d95b1c0920263
sukrittt <sukritsaha27@gmail.com>
2024-05-14 20:47:08 +0530

    upgrading packages

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,18 +1,11 @@
-// import { connect } from "@planetscale/database";
-// import { drizzle } from "drizzle-orm/planetscale-serverless";
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
-
-// import * as schema from "./schema";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is missing");
 }
 
-// create the connection
-// const connection = connect({
-//   url: process.env.DATABASE_URL,
-// });
-const client = postgres(process.env.DATABASE_URL);
+const connectionString = process.env.DATABASE_URL;
 
+export const client = postgres(connectionString, { prepare: false });
 export const db = drizzle(client);
