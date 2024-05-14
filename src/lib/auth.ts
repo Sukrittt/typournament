c36a382d5c188b43080d58622d121326adbcf4cd
sukrittt <sukritsaha27@gmail.com>
2024-05-14 19:20:07 +0530

    fix: fixing adapter types

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,10 @@
 import { eq } from "drizzle-orm";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
 
 import { db } from "~/db";
 import { users } from "~/db/schema";
-import { Adapter } from "next-auth/adapters";
 import { getServerSession, type NextAuthOptions } from "next-auth";
 
 function getGithubCredentials() {
@@ -39,7 +38,7 @@ function getGoogleCredentials() {
 }
 
 export const authOptions: NextAuthOptions = {
-  adapter: DrizzleAdapter(db) as Adapter,
+  adapter: DrizzleAdapter(db),
   session: {
     strategy: "jwt",
   },
